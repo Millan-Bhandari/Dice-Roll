e4c636715a84800bdbe8c0554d1c410606ce335d
Millan Bhandari <millan.ib@icloud.com>
2022-07-29 09:33:13 -0500

    Initial Commit

---
+++ Dice Roll/Dice_RollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollApp.swift
+//  Dice Roll
+//
+//  Created by Millan Bhandari on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_RollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
